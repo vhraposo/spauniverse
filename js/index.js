9a1004c6b619f43a38b6d35833409b6da9162afc
vhraposo <thevhup@gmail.com>
2023-03-11 14:38:34 -0300

    change bg on click

--- js/index.js
+++ js/index.js
@@ -10,3 +10,67 @@ router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+
+
+const universeScreen = document.querySelector('.universeScreen')
+const homeScreen = document.querySelector('.homeScreen')
+const explorationScreen = document.querySelector('.explorationScreen')
+
+
+const homeButton = document.querySelector('#homeButton')
+
+const universeButton = document.querySelector('#universeButton')
+
+const explorationButton = document. querySelector('#explorationButton')
+
+
+
+
+universeScreen.addEventListener('click', function(){
+    removeAllBgs()
+    document.querySelector('html').classList.add('bg2')
+})
+
+
+explorationScreen.addEventListener('click', function(){
+    removeAllBgs()
+    document.querySelector('html').classList.add('bg3')
+    
+})
+
+
+homeScreen.addEventListener('click', function(){
+    removeAllBgs()
+    document.querySelector('html').classList.add('bg1')
+    
+})
+
+function removeAllBgs(){
+    document.querySelector('html').classList.remove('bg1')
+    document.querySelector('html').classList.remove('bg2')
+    document.querySelector('html').classList.remove('bg3')
+    
+    
+}
+homeButton.addEventListener('click', function(){
+    removeAllWhiteColors()
+    homeButton.style.color = '#70709e'
+})
+
+universeButton.addEventListener('click', function(){
+    removeAllWhiteColors()
+    universeButton.style.color = '#70709eed'
+})
+
+explorationButton.addEventListener('click', function(){
+    removeAllWhiteColors()
+    explorationButton.style.color = '#70709e'
+})
+
+function removeAllWhiteColors(){
+    homeButton.style.color = '#E1E1E6'
+    universeButton.style.color = '#E1E1E6'
+    explorationButton.style.color = '#E1E1E6'
+}
+
+
